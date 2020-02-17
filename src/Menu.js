7919b7bc78d8f8fa50c18edb3b467e63267d369b
Annie <sharkeyconwaya@gmail.com>
2020-02-16 21:43:21 -0500

    removed first checkpoint comments

--- src/Menu.js
+++ src/Menu.js
@@ -8,7 +8,6 @@ const mainCourses = [
   { title: "Main Course 3", description: "Nice description woot 3", price: 10 },
   { title: "Main Course 3", description: "Nice description woot 3", price: 10 }
 ];
-//https://www.willhough.com/menu/
 
 const mainCourse = (title, description, price) => {
   return (
@@ -39,37 +38,3 @@ const Menu = () => {
 };
 
 export default Menu;
-
-// 1.
-// const title = "Main course";
-// const description = "Nice description of main course";
-// const price = "10";
-// const Menu = () => {
-//     return (
-//       <div>
-//         <header>Our Menu</header>
-//         {/* Card */}
-//         <div className="menuItem">
-//           <h2>{title}</h2>
-//           <h6>{description}</h6>
-//           <h6> .............. {price}</h6>
-//         </div>
-//       </div>
-//     );
-//   };
-
-//   export default Menu;
-
-//Then create main course const
-// const Menu = () => {
-//     return (
-//       <div>
-//         <header>Our Menu</header>
-//         <div>{mainCourse}</div>
-//         <div>{mainCourse}</div>
-//         <div>{mainCourse}</div>
-//       </div>
-//     );
-//   };
-
-//   export default Menu;
